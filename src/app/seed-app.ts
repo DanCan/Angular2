2bf141fddbef71374c6d90c2b7756a8ca6ddfd6f
DanCan <dancannova@gmail.com>
2016-06-16 18:09:04 -0700

    modified to remove not working router

--- src/app/seed-app.ts
+++ src/app/seed-app.ts
@@ -3,7 +3,6 @@ import {Routes, ROUTER_DIRECTIVES} from '@angular/router';
 
 import {Home} from './components/home/home';
 import {About} from './components/about/about';
-import {RepoBrowser} from './components/repo-browser/repo-browser';
 
 @Component({
   selector: 'seed-app',
@@ -15,8 +14,7 @@ import {RepoBrowser} from './components/repo-browser/repo-browser';
 @Routes([
   { path: '/',       component: Home,       },
   { path: '/about',  component: About,      },
-  { path: '/github', component: RepoBrowser },
-])
+  ])
 export class SeedApp {
 
   constructor() {}
